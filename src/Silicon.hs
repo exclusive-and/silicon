ac43300816160458a3828044f1ba962813362d0d
exclusive-and <exclusiveandgate@gmail.com>
2023-05-16 19:48:30 -0400

    Added application operators

--- src/Silicon.hs
+++ src/Silicon.hs
@@ -4,7 +4,30 @@
 -- Module       : Silicon
 -- Description  : Top-level Silicon Re-exporter
 -- 
-module Silicon (module X) where
+module Silicon
+    ( module X
+    , (|>), (<|)
+    ) where
 
 import Silicon.BitArithmetic as X
 import Silicon.Signal as X
+
+
+infixl 0 |>
+infixr 0 <|
+
+-- |
+-- Apply the second argument as a function to the first.
+-- 
+-- Equivalent to @'flip' '($)'@.
+-- 
+(|>) :: a -> (a -> b) -> b
+(|>) x f = f x
+
+-- |
+-- Apply the first argument as a function to the second.
+-- 
+-- Equivalent to @'($)'@.
+-- 
+(<|) :: (a -> b) -> a -> b
+(<|) f x = f x
