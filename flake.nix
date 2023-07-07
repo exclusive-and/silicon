eb743003251278ff8425bd8a1faaa090a3489abc
exclusive-and <exclusiveandgate@gmail.com>
2023-07-07 17:28:01 -0400

    One flake to rule them all

--- flake.nix
+++ flake.nix
@@ -10,28 +10,23 @@
     let
       config = {};
 
-      overlay = final: prev:
+      # Overlay to add our extra dependencies to the Nix Haskell package set.
+      haskOverlay = final: prev:
         {
           hask = final.haskell.packages.ghc96.override {
             overrides = haskfinal: haskprev: {
               base-compat = haskfinal.callHackage "base-compat" "0.13.0" {};
               lattices    = haskfinal.callHackage "lattices" "2.2" {};
-              silicon     = haskfinal.callCabal2nix "silicon" ./. {};
             };
           };
-
-          silicon = final.hask.silicon;
         };
-
-      overlays = [ overlay ];
 
       system = "x86_64-linux";
 
-      pkgs = import nixpkgs { inherit config system; overlays = overlays; };
+      pkgs = import nixpkgs
+        { inherit config system; overlays = [ haskOverlay ]; };
     in
     {
-      overlays.default = overlay;
-
       packages.${system}.default = pkgs.silicon;
     };
 }
