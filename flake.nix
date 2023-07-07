7378e74390c4ef9c9e7ebf4143952e9c46e21283
exclusive-and <exclusiveandgate@gmail.com>
2023-07-07 18:51:44 -0400

    Finalized flake

--- flake.nix
+++ flake.nix
@@ -8,29 +8,24 @@
 
   outputs = { self, nixpkgs }:
     let
-      config = {};
-
-      # Overlay to add our extra dependencies to the Nix Haskell package set.
+      # Overlay to manage dependencies in the Nix Haskell package set.
       haskOverlay = final: prev:
         {
           hask = final.haskell.packages.ghc96.override {
             overrides = haskfinal: haskprev: {
               base-compat = haskfinal.callHackage "base-compat" "0.13.0" {};
               lattices    = haskfinal.callHackage "lattices" "2.2" {};
-              silicon     = haskfinal.callCabal2nix "silicon" ./. {};
             };
           };
 
-          silicon = final.hask.silicon;
+          silicon = final.hask.callCabal2nix "silicon" ./. {};
         };
 
       system = "x86_64-linux";
-
-      pkgs = import nixpkgs
-        { inherit config system; overlays = [ haskOverlay ]; };
+      pkgs = import nixpkgs { inherit system; overlays = [ haskOverlay ]; };
     in
     {
-      overlay = haskOverlay;
+      overlays.default = haskOverlay;
 
       packages.${system}.default = pkgs.silicon;
     };
