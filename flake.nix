54086c42860ec040a1754dbf7d3d2b989f77fd82
exclusive-and <exclusiveandgate@gmail.com>
2023-09-01 12:18:25 -0400

    Added dev shell

--- flake.nix
+++ flake.nix
@@ -14,15 +14,17 @@
     let
         system = "x86_64-linux";
 
+        applicative-do-more = adm.packages.${system}.default;
+
         overlay = final: prev:
         {
             silicon-depends = final.haskell.packages.ghc96.override
             {
                 overrides = hself: hsuper:
                 {
-                    applicative-do-more = adm.packages.${system}.default;
-                    base-compat = hself.callHackage "base-compat" "0.13.0" {};
-                    lattices    = hself.callHackage "lattices" "2.2" {};
+                    applicative-do-more = applicative-do-more;
+                    base-compat = hself.base-compat_0_13_0;
+                    lattices = hself.lattices_2_2;
                 };
             };
         };
@@ -35,10 +37,20 @@
 
         silicon = pkgs.silicon-depends.callCabal2nix "silicon" ./. {};
 
+        silicon-env = pkgs.silicon-depends.shellFor
+        {
+            packages = _: [ silicon ];
+
+            nativeBuildInputs = [
+                pkgs.haskell.packages.ghc96.cabal-install
+                pkgs.haskell.packages.ghc96.haskell-language-server
+            ];
+        };
+
     in
     {
         overlays.default = overlay;
-
         packages.${system}.default = silicon;
+        devShells.${system}.default = silicon-env;
     };
 }
