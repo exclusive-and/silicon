9ea166e23173b91ae02cc7be7dbc9a2201824e70
exclusive-and <exclusiveandgate@gmail.com>
2023-09-01 11:37:32 -0400

    Use flakes lol

--- default.nix
+++
@@ -1,8 +0,0 @@
-
-# Package       : silicon
-# Description   : Software Simulation of Synchronous Digital Circuits
-# Build         : GHC 9.4.2 using Cabal2Nix
-
-{ nixpkgs ? import <nixpkgs> {} }:
-    with nixpkgs.haskell.packages.ghc942;
-    callCabal2nix "silicon" ./. {}
